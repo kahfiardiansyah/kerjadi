7d65d2950cb635ab415849ada867f6ab5ba37f37
kahfiardiansyah <kahfiardiansyahmuhammad@gmail.com>
2025-09-28 19:58:38 +0700

    Add files via upload

---
+++ projek web/js/firebase-config.js
@@ -0,0 +1,9 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyBd6vx174tjjVHTg1geqgZ2UZV_MIotIjQ",
+    authDomain: "kerjadi-web.firebaseapp.com",
+    projectId: "kerjadi-web",
+    storageBucket: "kerjadi-web.firebasestorage.app",
+    messagingSenderId: "390203624778",
+    appId: "1:390203624778:web:d00cdddc9f0868b64245b3",
+    measurementId: "G-E68Z0RD9GQ"
+  };
